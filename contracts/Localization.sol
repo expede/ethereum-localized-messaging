c037793f0b15491421abc8d21924f93f46657cfe
Brooklyn Zelenka <be.zelenka@gmail.com>
2019-01-08 15:48:35 -0800

    Update compiler version and conventions

--- contracts/Localization.sol
+++ contracts/Localization.sol
@@ -1,26 +1,26 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.5.2;
 
 /// @title Holds a simple mapping of codes to their text representations
 contract Localization {
-    mapping(bytes32 => string) internal dictionary_;
+    mapping(bytes32 => string) internal dictionary;
 
     constructor() public {}
 
     /// @notice Sets a mapping of a code to its text representation
-    /// @param _code The code with which to associate the text
-    /// @param _message The text representation
-    function set(bytes32 _code, string memory _message) internal {
-        dictionary_[_code] = _message;
+    /// @param code The code with which to associate the text
+    /// @param message The text representation
+    function set(bytes32 code, string memory message) internal {
+        dictionary[code] = message;
     }
 
-    function set(bytes32 _code, string storage _message) internal {
-      dictionary_[_code] = _message;
+    function set(bytes32 code, string storage message) internal {
+        dictionary[code] = message;
     }
 
     /// @notice Fetches the localized text representation.
-    /// @param _code The code to lookup
+    /// @param code The code to lookup
     /// @return The text representation for given code, or an empty string
-    function textFor(bytes32 _code) external view returns (string memory _message) {
-        return dictionary_[_code];
+    function textFor(bytes32 code) external view returns (string memory message) {
+        return dictionary[code];
     }
 }
